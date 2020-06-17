fc0b40924f8d6128063a4d0e618af23b35ec601c
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2020-06-17 22:49:47 +0900

    Enabled New Relic.

--- hello_openshift.go
+++ hello_openshift.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +26,14 @@ func listenAndServe(port string) {
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("NEW_RELIC_APP_NAME")),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_NAME")),
+		newrelic.ConfigDebugLogger(os.Stdout),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
+
+	http.HandleFunc(newrelic.WrapHandleFunc(app, "/", helloHandler))
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
